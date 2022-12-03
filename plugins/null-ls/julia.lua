1de7c0f5ee63c19ac9b96b9c845f9376bddad267
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2022-12-03 19:53:50 +0800

    use juliafmt

--- plugins/null-ls/julia.lua
+++ plugins/null-ls/julia.lua
@@ -1,24 +1,16 @@
 local h = require("null-ls.helpers")
 local methods = require("null-ls.methods")
 
-local FORMATTING = methods.internal.FORMATTING
-
-return h.make_builtin({
+return {
+    method = methods.internal.FORMATTING,
     name = "JuliaFormatter",
     meta = {
         url = "https://github.com/domluna/JuliaFormatter.jl",
-        description = "The uncompromising Python code formatter.",
+        description = "An opinionated code formatter for Julia.",
     },
-    method = FORMATTING,
     filetypes = { "julia" },
-    generator_opts = {
-        command = "julia",
-        args = {
-            "-e",
-            "using JuliaFormatter; println(format_text(String(read(stdin))))",
-        },
+    generator = h.formatter_factory {
+        command = "juliafmt",
         to_stdin = true,
-    timeout = 30000,
-    },
-    factory = h.formatter_factory,
-})
+    }
+}
