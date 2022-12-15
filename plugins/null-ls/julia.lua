20e9b26bb2c715ce5d5097baf5efaa9730b4e01e
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2022-12-15 15:25:16 +0800

    juliafmt use temp file

--- plugins/null-ls/julia.lua
+++ plugins/null-ls/julia.lua
@@ -1,16 +1,25 @@
-local h = require("null-ls.helpers")
-local methods = require("null-ls.methods")
+-- This null-ls source provides a formatter for JuliaLang.
+-- juliafmt is not provided by any packages, and it's just a simple
+-- bash script written by me. For more information, see
+-- https://allanchain.github.io/blog/post/julia-nvim/
+
+local h = require "null-ls.helpers"
+local methods = require "null-ls.methods"
 
 return {
-    method = methods.internal.FORMATTING,
-    name = "JuliaFormatter",
-    meta = {
-        url = "https://github.com/domluna/JuliaFormatter.jl",
-        description = "An opinionated code formatter for Julia.",
+  method = methods.internal.FORMATTING,
+  name = "JuliaFormatter",
+  meta = {
+    url = "https://github.com/domluna/JuliaFormatter.jl",
+    description = "An opinionated code formatter for Julia.",
+  },
+  filetypes = { "julia" },
+  generator = h.formatter_factory {
+    command = "juliafmt",
+    to_temp_file = true,
+    from_temp_file = true,
+    args = {
+      "$FILENAME",
     },
-    filetypes = { "julia" },
-    generator = h.formatter_factory {
-        command = "juliafmt",
-        to_stdin = true,
-    }
+  },
 }
