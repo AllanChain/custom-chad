d842ce17d76e25b7bfb5de9b6868fcf7d6c41e88
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-01-23 18:36:04 +0800

    auto select python path for pyright

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -2,6 +2,10 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
+local util = require('lspconfig/util')
+
+local path = util.path
+local exepath = util.exepath
 
 local servers = {
   "html",
@@ -25,9 +29,28 @@ lspconfig.tsserver.setup {
   end,
 }
 
+-- https://github.com/neovim/nvim-lspconfig/issues/500
+local function get_python_path(workspace)
+  -- Use activated virtualenv.
+  if vim.env.VIRTUAL_ENV then
+    return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+  end
+  -- Find and use virtualenv via poetry in workspace directory.
+  local match = vim.fn.glob(path.join(workspace, 'poetry.lock'))
+  if match ~= '' then
+    local venv = vim.fn.trim(vim.fn.system('poetry env info -p'))
+    return path.join(venv, 'bin', 'python')
+  end
+  -- Fallback to system Python.
+  return exepath "python3" or exepath "python" or "python"
+end
+
 lspconfig.pyright.setup {
   on_attach = on_attach,
   capabilities = capabilities,
+  before_init = function(_, config)
+    config.settings.python.pythonPath = get_python_path(config.root_dir)
+  end,
   settings = {
     python = {
       analysis = {
@@ -43,8 +66,8 @@ lspconfig.julials.setup {
   settings = {
     julia = {
       lint = {
-        missingrefs = "none"
-      }
-    }
-  }
+        missingrefs = "none",
+      },
+    },
+  },
 }
