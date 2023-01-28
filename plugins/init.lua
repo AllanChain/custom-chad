6848b452eaab1d41cb7676990da416242291c944
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-01-28 18:44:40 +0800

    load telescope in dashboard

--- plugins/init.lua
+++ plugins/init.lua
@@ -16,6 +16,7 @@ return {
   },
   ["NvChad/nvterm"] = false,
   ["nvim-telescope/telescope.nvim"] = {
+    ft = { "alpha" },
     override_options = {
       extensions_list = { "themes", "projects" },
     },
