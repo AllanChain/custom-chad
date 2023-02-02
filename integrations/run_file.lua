7a263446b93db94450cecc0c62265b6288ff7867
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-02-02 21:16:38 +0800

    use poetry run instead of poetry shell

--- integrations/run_file.lua
+++ integrations/run_file.lua
@@ -9,19 +9,18 @@ function M.run_file(c)
   if c.go_back == nil then
     c.go_back = true
   end
-  local term, created = terms.get_or_create_term(c.num, c.dir, c.direction)
+  local term = terms.get_or_create_term(c.num, c.dir, c.direction)
   local filetype = vim.filetype.match { filename = c.filename }
   if not term:is_open() then
     term:open(c.size, c.direction)
   end
-  if filetype == "python" and created then
-    local stat = vim.loop.fs_stat "poetry.lock"
-    if stat ~= nil then
-      term:send("poetry shell", false)
-    end
-  end
   if filetype == "python" then
-    term:send("python3 " .. c.filename, c.go_back)
+    local stat = vim.loop.fs_stat "poetry.lock"
+    if stat == nil then
+      term:send("python3 " .. c.filename, c.go_back)
+    else
+      term:send("poetry run python " .. c.filename, c.go_back)
+    end
   elseif filetype == "sh" then
     term:send("bash " .. c.filename, c.go_back)
   end
