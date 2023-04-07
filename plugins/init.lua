760b95dabaf16b71e9805466ae8f535608f523d3
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-04-07 22:34:54 +0800

    add noice ui

--- plugins/init.lua
+++ plugins/init.lua
@@ -82,6 +82,9 @@ return {
           "css",
           "html",
           "bash",
+          "markdown",
+          "markdown_inline",
+          "regex",
         },
         highlight = {
           enable = true,
@@ -157,6 +160,7 @@ return {
         "nvdash",
         "nvcheatsheet",
         "alpha",
+        "noice",
         "",
       },
     },
@@ -396,5 +400,35 @@ return {
   { -- global search and replace
     "nvim-pack/nvim-spectre",
     cmd = "Spectre",
-  }
+  },
+  {
+    "folke/noice.nvim",
+    lazy = false,
+    opts = {
+      lsp = {
+        signature = { enabled = false },
+        hover = { enabled = false },
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+    },
+    config = function(_, opts)
+      require("noice").setup(opts)
+    end,
+    dependencies = {
+      {
+        "MunifTanjim/nui.nvim",
+      },
+    },
+  },
 }
