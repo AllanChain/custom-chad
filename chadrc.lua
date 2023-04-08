562722afea16bd10d3b23459d2c859dd9b307312
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-04-08 15:29:38 +0800

    hide lsp progress on status line

--- chadrc.lua
+++ chadrc.lua
@@ -4,6 +4,15 @@ M.mappings = require "custom.mappings"
 
 M.ui = {
   theme = "onenord",
+  statusline = {
+    overriden_modules = function ()
+      return {
+        LSP_progress = function ()
+          return ""
+        end
+      }
+    end
+  }
 }
 
 M.plugins = "custom.plugins"
