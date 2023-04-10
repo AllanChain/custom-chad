31d1bae7dcc879ca95b96fa7d2562d746f5d2f0f
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-04-10 17:45:41 +0800

    ensure cwd is added to session projects

--- plugins/init.lua
+++ plugins/init.lua
@@ -264,6 +264,15 @@ return {
         detection_methods = { "pattern", "lsp" },
         silent_chdir = false,
       }
+      local p = require "project_nvim.project"
+      local history = require "project_nvim.utils.history"
+      -- ensure current dir is added to the session
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          local root, _ = p.get_project_root()
+          table.insert(history.session_projects, root)
+        end,
+      })
     end,
   },
   {
