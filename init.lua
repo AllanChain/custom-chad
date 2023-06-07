5b16e353ea357302c1162789e4b97a954b08b738
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-06-07 15:47:03 +0800

    trigger autoread on focus gain

--- init.lua
+++ init.lua
@@ -8,6 +8,7 @@ if not vim.loop.fs_stat(lazypath) then
   require("custom.bootstrap").lazy(lazypath)
 end
 
+-- setup colorcolumn based on editorconfig
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern="*",
   callback=function ()
@@ -17,5 +18,14 @@ vim.api.nvim_create_autocmd("BufEnter", {
     local tw = config.max_line_length
     if tw == nil then return end
     vim.api.nvim_win_set_option(0, "colorcolumn", tw)
+  end
+})
+
+-- better autoread on file change
+-- https://github.com/neovim/neovim/issues/1936#issuecomment-309311829
+vim.api.nvim_create_autocmd("FocusGained", {
+  pattern = "*",
+  callback = function ()
+    vim.cmd("checktime") -- checktime triggers autoread
   end
 })
