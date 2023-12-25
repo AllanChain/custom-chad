27ea6fb5d434b149bdc2630293a7432cb5f45f7b
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-12-25 16:53:39 +0800

    auto osc52 copy

--- plugins/init.lua
+++ plugins/init.lua
@@ -235,9 +235,14 @@ return {
       if not ok then
         return
       end
-      osc52.setup {
-        silent = true,
-      }
+      osc52.setup { silent = true }
+      vim.api.nvim_create_autocmd("TextYankPost", {
+        callback = function()
+          if vim.env.TMUX and vim.v.event.operator == "y" and vim.v.event.regname == "+" then
+            osc52.copy_register "+"
+          end
+        end,
+      })
     end,
   },
   {
